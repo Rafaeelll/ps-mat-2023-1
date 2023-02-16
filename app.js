ab08bb1ea5906b11ccbc446578ef9d0c19943e95
Rafael Felipe <89078382+Rafaeelll@users.noreply.github.com>
2023-02-16 14:03:40 +0000

    (16/02) Criação do primeiro model e migration

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// Carrega as variaveis de ambiente do arquivo
+//.env para aplicação
+require('dotenv').config()
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -7,7 +11,19 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+ // conecta ao BD
 
+ const db = require('./models')
+
+ try {
+    db.sequelize.authenticate()
+console.log('SEQUELIZE: connection has been established sucessfully.')
+
+ }
+ catch(error){
+    console.log('* SEQUELIZE: unable to connect to the database: ', error)
+    process.exit(1)
+ }
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
