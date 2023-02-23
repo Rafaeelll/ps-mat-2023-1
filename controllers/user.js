e17dd400909514794941c5483d42914107838cc5
Rafael Felipe <89078382+Rafaeelll@users.noreply.github.com>
2023-02-23 14:08:23 +0000

    (23/02) Primeiro controller e rota

---
+++ controllers/user.js
@@ -0,0 +1,25 @@
+// importar o model correspondente ao controller
+const {User} = require('../models')
+
+const controller = {} // objeto vazio
+
+/*
+    metodos CRUD do controller
+    create: Cria um novo registro
+    retrieve: lista (recupera) todos os registros
+    retrieveOne: Lista (recupera) um registro
+    uptade: atualiza um registro
+    delete: deletar um registro
+*/
+
+controller.create = async (req, res) =>{
+    try{
+        await User.create(req.body)
+        // HTTP 201: Created
+        res.status(201).end()
+    }
+    catch(error){
+        console.error(error)
+    }
+}
+module.exports = controller
